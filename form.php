86d82e6c02fe31dc95285c4104fdb44ffed2a4ec
mehreenraza5 <razamehreen08@gmail.com>
2025-08-14 22:03:17 +0500

    Updated contact form design and fixed responsive issues

--- form.php
+++ form.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@universityportal.com';
+$email_from = 'mahiraza401@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User_name: $name.\n".
              "User_email: $visitor_email.\n".
@@ -12,7 +12,7 @@ $email_body = "User_name: $name.\n".
              "User_message: $message.\n";
 $to = 'razamehreen08@gmail.com';
 $headers ="From: $email_from \r\n";
-$headers. = "Reply-To: $visitor_email \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
 
 if (mail($to, $email_subject, $email_body, $headers)) {
         header("Location: contact.html");
